1f99fa13a3cdd6aafa603c8e120cb4955736af68
DESKTOP-H3FHK29\tyler <tyler.da55@gmail.com>
2025-03-26 20:47:25 +1100

    wip

---
+++ README.txt
@@ -0,0 +1,14 @@
+#######################
+    Python funtions
+#######################
+
+requires pip install pyinstaller to create an executable Flask app which is used by the Electron app:
+- pyinstaller -F app.py
+
+##########################
+    Electron Functions
+##########################
+Use npm start to run locally
+
+Uses electron-forge to create an executable:
+- npm run make
---
+++ src/services/backend.service.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export class BackendService {
+    BASE_URL: string = 'http://127.0.0.1:5000';
+    
+    async sendAxios() {
+        axios.get(this.BASE_URL.concat('/test'))
+          .then(response => {
+            console.log("It says: ", response.data);
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      }
+  }
